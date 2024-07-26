376c73624222d13f7dabd02a0ef24bbca08ac012
MurilloRBS <104856741+MurilloRBS@users.noreply.github.com>
2024-07-26 13:34:41 +0000

    g

---
+++ main.js
@@ -0,0 +1,36 @@
+const perguntas = [
+    pergunta1{
+        enunciado: "Você gosta de bolo?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "Você é uma pessoa normal."
+            }, 
+            {
+                texto: "não",
+                afirmacao: "Você é chato pra caralho."
+            }]
+
+    },
+    pergunta2{
+        enunciado: "Você dorme?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "Você é uma pessoa normal."
+            }, 
+            {
+                texto: "não",
+                afirmacao: "Você é esquisito."
+            }]
+
+    }
+    
+]
+
+
+
+
+function mostraPergunta(){
+    
+}
